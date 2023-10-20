4278ea14085526b1850f4671565de19bc42afb7d
RoyalPandalfuul <61811141+RoyalPandalfuul@users.noreply.github.com>
2023-10-20 20:45:47 +0000

    add: new user schema and db model

---
+++ artferia/models.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str]
+    password: Mapped[str]
+    email: Mapped[str]
