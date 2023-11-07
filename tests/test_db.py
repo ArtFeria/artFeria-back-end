b9e238f8a6934b571ed7e4b3e5bdf7dca6ea386f
RoyalPandalfuul <61811141+RoyalPandalfuul@users.noreply.github.com>
2023-11-07 23:27:42 +0000

    add: new test_create_event

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import select
 
-from artferia.models import User
+from artferia.models import Event, User
 
 
 def test_db_create_user(session):
@@ -11,3 +11,20 @@ def test_db_create_user(session):
     user = session.scalar(select(User).where(User.username == 'alice'))
 
     assert user.username == 'alice'
+
+
+def test_db_create_event(session, user):
+    new_event = Event(
+        name='rock show',
+        age=16,
+        description='best rock show ever!',
+        user_id=user.id,
+    )
+
+    session.add(new_event)
+    session.commit()
+
+    event = session.scalar(select(Event).where(Event.user_id == user.id))
+
+    assert event.name == 'rock show'
+    assert event.description == 'best rock show ever!'
