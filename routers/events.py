73025f42b8145000c868c54cc21f1f6abe8bbb37
RoyalPandalfuul <61811141+RoyalPandalfuul@users.noreply.github.com>
2023-11-07 23:26:08 +0000

    add: new event route add events to db

--- routers/events.py
+++ routers/events.py
@@ -1,28 +1,41 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends
 
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
-from artferia.models import Event
-from artferia.schemas import EventSchema, Message
 from artferia.database import get_session
+from artferia.models import Event, User
+from artferia.schemas import EventSchema, Message
 from artferia.security import get_current_user
 
 router = APIRouter(prefix='/events', tags=['events'])
 
 Session = Annotated[Session, Depends(get_session)]
+CurrentUser = Annotated[User, Depends(get_current_user)]
+
 
 @router.get('/', response_model=Message)
-def read_events():
+def read_router():
     return {'detail': 'hello events!'}
 
 
-@router.post('/', response_model=EventSchema)
-def create_event(event: EventSchema, session: Session):
+@router.post('/', status_code=201, response_model=EventSchema)
+def create_event(
+    event: EventSchema, session: Session, current_user: CurrentUser
+):
     """create one event"""
+    has_event = session.scalar(select(Event).where(Event.name == event.name))
+    if has_event:
+        raise HTTPException(status_code=401, detail='event already exists')
 
-    session.add(event)
+    db_event = Event(
+        name=event.name,
+        age=event.age,
+        description=event.description,
+        user_id=current_user.id,
+    )
+
+    session.add(db_event)
     session.commit()
-    session.refresh(event)
     return event
